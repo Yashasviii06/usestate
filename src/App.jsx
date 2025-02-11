d164e36d19f0e622b923d6a97bb4c7e5a26121cc
Yashasvi <167102105+Yashasviii06@users.noreply.github.com>
2025-02-11 23:46:58 +0530

    usestate(example choco)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import Comp1, { Comp2 } from "./components/Comp1";
+import { useState } from "react";
+// import Comp1, { Comp2 } from "./components/Comp1";
 
 
 const App=()=>
 {
+    const[choco , setchoco]= useState(20);
+    function sellfun()
+    {
+      if(choco>0)
+      {
+        setchoco(choco-1);
+      }
+      else{
+        alert("out of stock");
+      }
+    }
     //js logic code
   return(
     <>
-         <h1> hello react App Component</h1>
-         <p>this is paragraph from App Component</p>
-         <Comp1 />
+         <h1> chocolate shop</h1>
+         <p>available chocolate: {choco}</p>  
+         <button onClick={sellfun}>sell 1 chcolate</button>
+
+         {/* <Comp1 />
          <Comp2 />
          <h1> star</h1>
-         <p>moon</p>
+         <p>moon</p> */}
+
+
+
+
+
     </>
     
   );
